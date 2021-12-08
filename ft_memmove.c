82933e22dc476188a859a6db9b5508e3c922958d
Jinsol Kwak <jkwak@c2r10s5.42seoul.kr>
2021-12-08 16:01:09 +0900

    211208

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,26 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jkwak <jkwak@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/25 20:26:19 by jkwak             #+#    #+#             */
-/*   Updated: 2021/12/08 14:28:01 by jkwak            ###   ########.fr       */
+/*   Created: 2021/12/08 14:27:45 by jkwak             #+#    #+#             */
+/*   Updated: 2021/12/08 16:00:55 by jkwak            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	size_t	i;
 
+	if (!dst || !src)
+		return (NULL);
 	i = 0;
-	while (i < n)
+	if (dst <= src)
 	{
-		*((char *)dst + i) = *((char *)src + i);
-		i++;
+		while (i < len)
+		{
+			*((char *)dst + i) = *((char *)src + i);
+			i++;
+		}
+	}
+	else
+	{
+		i = len - 1;
+		while (i >= 0)
+		{
+			*((char *)dst + i) = *((char *)src + i);
+			i--;
+		}
 	}
 	return (dst);
 }
--- libft.h
+++ libft.h
@@ -1,26 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jkwak <jkwak@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/25 20:26:19 by jkwak             #+#    #+#             */
-/*   Updated: 2021/12/08 14:28:01 by jkwak            ###   ########.fr       */
+/*   Created: 2021/12/08 13:02:31 by jkwak             #+#    #+#             */
+/*   Updated: 2021/12/08 13:15:03 by jkwak            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#ifndef LIBFT_H
+# define LIBFT_H
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
-{
-	size_t	i;
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
 
-	i = 0;
-	while (i < n)
-	{
-		*((char *)dst + i) = *((char *)src + i);
-		i++;
-	}
-	return (dst);
-}
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+
+#endif
